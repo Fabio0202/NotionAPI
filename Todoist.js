1cff712d5e04fc96b745808357ff31b671dfa72c
fabiomuscas <fabiomuscas@gmail.com>
2024-12-30 12:22:03 +0100

    get New Task and log content every minute

--- Todoist.js
+++ Todoist.js
@@ -30,4 +30,45 @@ async function getProjects() {
     }
 }
 
-module.exports = { createInboxTask, getProjects };
+// eine funktion die eine andere funktion jede minute abruft
+async function runEveryMinute(fn) {
+    await fn();
+    setTimeout(() => runEveryMinute(fn), 60000);
+}
+
+// eine funktion die alle tasks aus dem inbox project holt
+async function getInboxTasks() {
+    const tasks = await api.getTasks({projectId: projects[0].id});
+    return tasks
+}
+
+function filterOnlyNewTasks(tasks) {
+    const OneMinuteAgo = getOneMinuteAgo();
+    return tasks.filter(task => new Date(task.createdAt) > OneMinuteAgo);
+}
+
+// eine funktion die das jetzige datum minus eine minute ausgibt
+function getOneMinuteAgo() {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() - 1);
+    return now;
+}
+
+
+async function getAllNewTasksEveryMinute() {
+    const functionToRun = async () => {
+        
+        const allTasks = await getInboxTasks();
+        const filteredTasks = filterOnlyNewTasks(allTasks);
+        filteredTasks.map(task => console.log(task.content));
+    }
+
+    runEveryMinute(functionToRun);
+}
+
+module.exports = { createInboxTask, getProjects,
+
+    runEveryMinute, getInboxTasks,  getAllNewTasksEveryMinute, getOneMinuteAgo
+ };
+
+ //wenn Task returned, dann rufe Notion create Page auf
--- main.js
+++ main.js
@@ -3,4 +3,5 @@ const todoist = require('./Todoist.js');
 
 require('dotenv').config();
 // getPokemon();
-todoist.createInboxTask({content: "Hallo ich bin ein test"});
+// todoist.createInboxTask({content: "Hallo ich bin ein test"});
+todoist.getAllNewTasksEveryMinute()
